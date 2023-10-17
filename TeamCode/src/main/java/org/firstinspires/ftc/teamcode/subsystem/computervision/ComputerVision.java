761cee68937fd07fd6c02397238248824a44b249
Jeffrey <jeffreytvedt5@gmail.com>
2023-10-17 07:55:05 -0400

    Modified ComputerVision class into AbstractClass and added methods

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/computervision/ComputerVision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/computervision/ComputerVision.java
@@ -1,10 +1,37 @@
 package org.firstinspires.ftc.teamcode.subsystem.computervision;
 
-import org.openftc.apriltag.AprilTagDetection;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 
 import java.util.*;
 
-public interface ComputerVision {
-    int getSpikeMark();
-    List<AprilTagDetection> getDetections();
+public abstract class ComputerVision {
+    private AprilTagProcessor aprilTag;
+
+    public abstract int getSpikeMark();
+
+    public ComputerVision(HardwareMap hardwareMap) {
+        initAprilTag(hardwareMap);
+    }
+
+    public List<AprilTagDetection> getDetections() {
+        return aprilTag.getDetections();
+    }
+
+    /**
+     * Thank you arjun. Have fun!
+     * @param hardwareMap hardware map to get camera from
+     */
+    public void initAprilTag(HardwareMap hardwareMap) {
+        aprilTag = new AprilTagProcessor.Builder().build();
+
+        VisionPortal visionPortal = new VisionPortal.Builder()
+                .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
+                .addProcessor(aprilTag).build();
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/computervision/SampleCV.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/computervision/SampleCV.java
@@ -1,17 +1,19 @@
 package org.firstinspires.ftc.teamcode.subsystem.computervision;
 
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 import org.openftc.apriltag.AprilTagDetection;
 
 import java.util.List;
 
-public class SampleCV implements ComputerVision {
-    @Override
-    public int getSpikeMark() {
-        return 0;
+public class SampleCV extends ComputerVision {
+    public SampleCV(HardwareMap hardwareMap) {
+        super(hardwareMap);
     }
 
     @Override
-    public List<AprilTagDetection> getDetections() {
-        return null;
+    public int getSpikeMark() {
+        return 1;
     }
+
 }

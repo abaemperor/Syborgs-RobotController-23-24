a94ca2e189afe0ec162c83f8b5ef3a466b35e63a
Jeffrey <jeffreytvedt5@gmail.com>
2023-11-09 11:53:05 -0500

    Expanded ComputerVision code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/computervision/SampleCV.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/computervision/SampleCV.java
@@ -2,18 +2,42 @@ package org.firstinspires.ftc.teamcode.subsystem.computervision;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-import org.openftc.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+import org.openftc.easyopencv.OpenCvCamera;
 
 import java.util.List;
 
-public class SampleCV extends ComputerVision {
+public class SampleCV implements ComputerVision {
+    private HardwareMap hardwareMap;
+    private VisionPortal visionPortal;
+    private AprilTagProcessor aprilTag;
+    private OpenCvCamera camera;
+    private SampleCVProcessor processor;
+
     public SampleCV(HardwareMap hardwareMap) {
-        super(hardwareMap);
+        this.hardwareMap = hardwareMap;
+
+        processor = new SampleCVProcessor();
+
+        aprilTag = new AprilTagProcessor.Builder().build();
+        aprilTag.setDecimation(2);
+
+        visionPortal = new VisionPortal.Builder()
+                .setCamera(hardwareMap.get(WebcamName.class, "Camera"))
+                .addProcessor(aprilTag)
+                .build();
     }
 
     @Override
     public int getSpikeMark() {
-        return 1;
+        return processor.getSpikeMark();
     }
 
+    @Override
+    public List<AprilTagDetection> getDetections() {
+        return aprilTag.getDetections();
+    }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/computervision/SampleCVProcessor.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.subsystem.computervision;
+
+import android.graphics.Canvas;
+
+import org.firstinspires.ftc.robotcore.internal.camera.calibration.CameraCalibration;
+import org.firstinspires.ftc.vision.VisionProcessor;
+import org.opencv.core.Mat;
+
+public class SampleCVProcessor implements VisionProcessor {
+    private int spikeMark = 0;
+
+    public int getSpikeMark() {
+        return spikeMark;
+    }
+
+    @Override
+    public void init(int width, int height, CameraCalibration calibration) {
+
+    }
+
+    @Override
+    public Object processFrame(Mat frame, long captureTimeNanos) {
+        return null;
+    }
+
+    @Override
+    public void onDrawFrame(Canvas canvas, int onscreenWidth, int onscreenHeight, float scaleBmpPxToCanvasPx, float scaleCanvasDensity, Object userContext) {
+
+    }
+}

de8350af66f0851cce90b4a09cd44b899cdecaf1
niero369 <subscribe2niero@gmail.com>
2023-11-25 15:41:41 -0500

    Created ETest & Updated TestTeleOp ETest: DPad controls TestTeleOp: tyler's code that he had me download cuz he needed to build to the dummy chassis while his AS was booting

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/EliotTestTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/EliotTestTeleOp.java
@@ -2,13 +2,37 @@ package org.firstinspires.ftc.teamcode.opmode.teleop;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
-@TeleOp
+import org.firstinspires.ftc.teamcode.subsystem.drivetrain.SampleMecanumDrive;
+
+@TeleOp(name = "womp womp")
 public class EliotTestTeleOp extends LinearOpMode {
+
+    SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
     @Override
     public void runOpMode() {
+        waitForStart();
+        while (opModeIsActive()) {
+            // DPad inputs
+            if (gamepad1.dpad_up) {
+                drive.drive(5);
+            }
+            if (gamepad1.dpad_down) {
+                drive.drive(-5);
+            }
+            if (gamepad1.dpad_right) {
+                drive.strafe(5);
+            }
+            if (gamepad1.dpad_left) {
+                drive.strafe(-5);
+            }
 
+            // Stick drive inputs
+
+
+        }
     }
 
 }

6657902f91259b65cfc122f91d741282caa66549
niero369 <subscribe2niero@gmail.com>
2023-11-25 14:39:50 -0500

    Created for testing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/EliotTestTeleOp.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.opmode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class EliotTestTeleOp extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+
+    }
+
+}

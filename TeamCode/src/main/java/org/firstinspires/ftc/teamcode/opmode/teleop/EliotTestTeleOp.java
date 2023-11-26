b71a30a76cc7b29fa24634d9a1c1c45c45125a2f
niero369 <subscribe2niero@gmail.com>
2023-11-25 23:42:58 -0500

    added double stickX and stickY variables for stick movement soon wrote code for stick movement and spin, will test later (also if anyone is reading this code and sees blatant problems pls blast me in your rewriting commit message lmao)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/EliotTestTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/EliotTestTeleOp.java
@@ -11,11 +11,12 @@ public class EliotTestTeleOp extends LinearOpMode {
 
     SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
-    double stickX = gamepad1.left_stick_x;
-    double stickY = -gamepad1.left_stick_y;
+    double LstickX = gamepad1.left_stick_x;
+    double LstickY = -gamepad1.left_stick_y;
     //reminder that the left_stick_y float corresponds as
     // -1 = up and 1 = down, so I just reversed them here
     // cuz I'm totally gonna forget later
+    double RstickX = gamepad1.right_stick_x;
 
     @Override
     public void runOpMode() {
@@ -36,9 +37,12 @@ public class EliotTestTeleOp extends LinearOpMode {
             }
 
             // Stick drive inputs
-            //drive.cartesianMove(stickX, stickY);
-            telemetry.addData("Gamepad stick X", stickX);
-            telemetry.addData("Gamepad stick Y", stickY);
+            drive.cartesianMove(5*LstickX, 5*LstickY);
+            drive.spin(0.2*RstickX);
+            // TODO: figure out proper coefficients to all these inputs
+            // i'm gonna be honest i have no clue if this works
+            // and/or if this is drivable but i can't test this rn
+            // so ima hope i understand the sample code lmao
 
         }
     }

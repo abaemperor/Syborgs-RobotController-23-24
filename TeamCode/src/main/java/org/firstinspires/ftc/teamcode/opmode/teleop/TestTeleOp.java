84f7d19d8f285d9042926cd457dc2ea30f70a9a0
HackerGuy1000 <107278740+HackerGuy1000@users.noreply.github.com>
2023-11-26 18:14:22 -0500

    Started Working On Telop Code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/TestTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/TestTeleOp.java
@@ -3,10 +3,15 @@ package org.firstinspires.ftc.teamcode.opmode.teleop;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.subsystem.drivetrain.SampleMecanumDrive;
+
 @TeleOp
 public class TestTeleOp extends LinearOpMode {
     @Override
     public void runOpMode() {
-        
+        SampleMecanumDrive chassis = new SampleMecanumDrive(hardwareMap);
+        chassis.drive(gamepad1.left_stick_y);
+        chassis.strafe(gamepad1.left_stick_x);
+        chassis.spin(gamepad1.right_stick_x);
     }
 }

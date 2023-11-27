9d22dabca258d34aa517304906b3966e3c6d5f3d
Jeffrey <jeffreytvedt5@gmail.com>
2023-11-27 08:44:21 -0500

    Created Math utils

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/math/Vector.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode.util.math;
+
+public class Vector {
+    private double x;
+    private double y;
+
+    public Vector(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() { return x; }
+    public double getY() { return y; }
+
+    public void setX(double x) { this.x = x; }
+    public void setY(double y) { this.y = y; }
+
+    public Vector rotate(double theta) {
+        Vector v = this;
+
+        // Basically just matrix multiplication
+        x = v.x * Math.cos(theta) + v.y * Math.cos(theta + MathUtils.HALF_PI);
+        y = v.x * Math.sin(theta) + v.y * Math.sin(theta + MathUtils.HALF_PI);
+
+        return this;
+    }
+
+    public Vector stretch(double x, double y) {
+        this.x *= x;
+        this.y *= y;
+        return this;
+    }
+
+    public Vector multiply(double magnitude) {
+        return this.stretch(magnitude, magnitude);
+    }
+}

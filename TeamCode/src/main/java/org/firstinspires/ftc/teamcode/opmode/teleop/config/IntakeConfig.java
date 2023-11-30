9562ed9fc428dc3cddf920d0fb8ad75d5c54d1b6
HackerGuy1000 <107278740+HackerGuy1000@users.noreply.github.com>
2023-11-30 11:04:21 -0500

    Added configuration options for the motor for lifting the servo and the continuous servo for rotating the gecko wheels

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/config/IntakeConfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/config/IntakeConfig.java
@@ -2,11 +2,55 @@ package org.firstinspires.ftc.teamcode.opmode.teleop.config;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
+import org.firstinspires.ftc.teamcode.controller.Controller;
+/**
+ * TeleOp for configuring the intake which consists of a servo and a motor
+ * @author Tyler Philip
+ * */
 @TeleOp(name="Config Intake")
 public class IntakeConfig extends LinearOpMode {
+    public DcMotor intakeLift;
+    public CRServo intakeSpin;
     @Override
     public void runOpMode(){
+        Controller controller = new Controller(gamepad1);
 
+        intakeLift = hardwareMap.get(DcMotor.class,"IL");
+        intakeSpin = hardwareMap.get(CRServo.class,"IS");
+
+        boolean spinning = false;
+
+        waitForStart();
+
+        while(opModeIsActive()){
+
+            //Allows the user to reverse the direction of the motors for the arm
+            if(controller.pressingButton("A")){
+                intakeLift.setDirection(DcMotor.Direction.REVERSE);
+            }
+            else if(controller.pressingButton("B")){
+                intakeLift.setDirection(DcMotor.Direction.FORWARD);
+            }
+            if(controller.pressingButton("RT")){
+                intakeSpin.setDirection(CRServo.Direction.REVERSE);
+            }
+            if(controller.pressingButton("LT")){
+                intakeSpin.setDirection(CRServo.Direction.FORWARD);
+            }
+
+
+            if(controller.pressingButton("X")){spinning = true;}
+
+            else if(controller.pressingButton("Y")){spinning=false;}
+
+            if(spinning){
+                intakeSpin.setPower(1);
+            }
+
+
+        }
     }
 }

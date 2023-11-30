4a2cc688984ff3658e133a766731cc35b6a6ffe1
HackerGuy1000 <107278740+HackerGuy1000@users.noreply.github.com>
2023-11-30 12:19:54 -0500

    Added functionality to modify the angle of the claw

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/config/ClawConfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/config/ClawConfig.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.subsystem.claw;
+package org.firstinspires.ftc.teamcode.opmode.teleop.config;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -6,21 +6,32 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.controller.Controller;
 
-@TeleOp(name = "Claw Config")
+/**
+* TeleOp to test the values of the left and right claw open and close positions
+* @author Tyler Philip
+* */
+
+@TeleOp(name = "Config Claw")
 public class ClawConfig extends LinearOpMode {
 
     public Servo leftClaw;
     public Servo rightClaw;
+
+    public Servo clawSpin;
 
     public double leftCloseValue = 0.7;
     public double rightCloseValue = 0.3;
     public double leftOpenValue = 1.0;
     public double rightOpenValue = 0.0;
 
+    public double spinDown = 0.0;
+    public double spinBack = 1.0;
+
     @Override
     public void runOpMode() {
         leftClaw = hardwareMap.get(Servo.class, "LC");
         rightClaw = hardwareMap.get(Servo.class, "RC");
+        clawSpin = hardwareMap.get(Servo.class,"CS");
 
         Controller controller = new Controller(gamepad1);
 
@@ -34,6 +45,12 @@ public class ClawConfig extends LinearOpMode {
             if (controller.pressingButton("Y")) { //Open claw based on the open values
                 leftClaw.setPosition(leftOpenValue);
                 rightClaw.setPosition(rightOpenValue);
+            }
+            if(controller.pressingButton("DU")){
+                clawSpin.setPosition(spinBack);
+            }
+            if(controller.pressingButton("DD")){
+                clawSpin.setPosition(spinDown);
             }
 
             if(controller.holdingButton("A")){
@@ -49,7 +66,14 @@ public class ClawConfig extends LinearOpMode {
                 if(controller.pressingButton("LB")){ //Left bumper and holding down A to increase the left open value
                     leftOpenValue += 0.1;
                 }
+                if(controller.pressingButton("DR")){
+                    spinBack += 0.1;
+                }
+                if(controller.pressingButton("DL")){
+                    spinDown += 0.1;
+                }
             }
+
             else if(controller.holdingButton("B")){
                 if(controller.pressingButton("RT")){  //Right trigger and holding down B to decrease right close value
                     rightCloseValue -= 0.1;
@@ -62,6 +86,12 @@ public class ClawConfig extends LinearOpMode {
                 }
                 if(controller.pressingButton("LB")){ //Left bumper and holding down B to decrease left open value
                     leftOpenValue -= 0.1;
+                }
+                if(controller.pressingButton("DR")){
+                    spinBack -= 0.1;
+                }
+                if(controller.pressingButton("DL")){
+                    spinDown -= 0.1;
                 }
             }
 

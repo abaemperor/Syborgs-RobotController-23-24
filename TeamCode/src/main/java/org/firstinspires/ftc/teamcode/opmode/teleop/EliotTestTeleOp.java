64a2b610e58fdb76f3bd749210be1364f4203175
HackerGuy1000 <107278740+HackerGuy1000@users.noreply.github.com>
2023-11-30 11:13:44 -0500

    Created class for mechanum drive in teleop and modified EliotTestTeleOp to use that class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/EliotTestTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/EliotTestTeleOp.java
@@ -2,45 +2,24 @@ package org.firstinspires.ftc.teamcode.opmode.teleop;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import org.firstinspires.ftc.teamcode.controller.Controller;
+
+/**
+ * Teleop made for testing purposes
+ * @author Eliot Bih and Tyler Philip
+ * */
 
 @TeleOp(name = "[TEST] Eliot TeleOp")
 public class EliotTestTeleOp extends LinearOpMode {
-    public DcMotor FR;
-    public DcMotor FL;
-    public DcMotor BR;
-    public DcMotor BL;
-
-    public void teleOpMap() {
-
-        FR = hardwareMap.get(DcMotor.class, "FR");
-        FR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        FL = hardwareMap.get(DcMotor.class, "FL");
-        FL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        BR = hardwareMap.get(DcMotor.class, "BR");
-        BR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        BL = hardwareMap.get(DcMotor.class, "BL");
-        BL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
-        //motor reverse for right side
-        FR.setDirection(DcMotorSimple.Direction.REVERSE);
-        BR.setDirection(DcMotorSimple.Direction.REVERSE);
-    }
-
-    public void teleDrive(double x, double y, double turn) {
-        telemetry.addData("Drive Input X", x);
-        telemetry.addData("Drive Input Y", y);
-
-        FL.setPower(x + y - turn);
-        FR.setPower(x - y + turn);
-        BL.setPower(x - y - turn);
-        BR.setPower(x + y + turn);
-    }
 
     public void runOpMode() {
-
-
+        TeleOpDrive chassis = new TeleOpDrive(hardwareMap);
+        Controller controller = new Controller(gamepad1);
+        waitForStart();
+        while(opModeIsActive()){
+            chassis.teleDrive(controller.getValue("LX"),controller.getValue("LY"), controller.getValue("RX"),telemetry);
+        }
     }
 
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/TeleOpDrive.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode.opmode.teleop;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class TeleOpDrive{
+    //Creates 4 variables to represent the 4 DcMotors
+    public DcMotor FR;
+    public DcMotor FL;
+    public DcMotor BR;
+    public DcMotor BL;
+    public TeleOpDrive(HardwareMap hardwareMap){
+        FR = hardwareMap.get(DcMotor.class, "FR");
+        FR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        FL = hardwareMap.get(DcMotor.class, "FL");
+        FL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        BR = hardwareMap.get(DcMotor.class, "BR");
+        BR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        BL = hardwareMap.get(DcMotor.class, "BL");
+        BL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        //motor reverse for right side
+        FR.setDirection(DcMotorSimple.Direction.REVERSE);
+        BR.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+    public void teleDrive(double x, double y, double spin, Telemetry telemetry){
+        telemetry.addData("Drive Input X", x);
+        telemetry.addData("Drive Input Y", y);
+
+        FL.setPower(x + y - spin);
+        FR.setPower(x - y + spin);
+        BL.setPower(x - y - spin);
+        BR.setPower(x + y + spin);
+
+        telemetry.update();
+    }
+}

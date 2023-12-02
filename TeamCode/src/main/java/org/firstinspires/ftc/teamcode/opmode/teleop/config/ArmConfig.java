49b298120b4b8bb7f6eebfcb9d5a21427180cc4b
HackerGuy1000 <107278740+HackerGuy1000@users.noreply.github.com>
2023-12-01 22:55:32 -0500

    Created the arm config program to determine specific positions for the motor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/config/ArmConfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/config/ArmConfig.java
@@ -4,12 +4,33 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+import org.firstinspires.ftc.teamcode.controller.Controller;
+
 @TeleOp(name="Config Arm")
 public class ArmConfig extends LinearOpMode {
     public DcMotor armMotor;
 
     @Override
     public void runOpMode(){
+        Controller controller = new Controller(gamepad1);
 
+        armMotor = hardwareMap.get(DcMotor.class, "AM");
+
+        armMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        waitForStart();
+
+        while(opModeIsActive()){
+            if(controller.pressingButton("A")){
+                armMotor.setDirection(DcMotor.Direction.REVERSE);
+            }
+            else if(controller.pressingButton("B")){
+                armMotor.setDirection(DcMotor.Direction.FORWARD);
+            }
+            armMotor.setPower(controller.getValue("LY"));
+
+            telemetry.addData("Current Position",armMotor.getCurrentPosition());
+            telemetry.update();
+        }
     }
 }

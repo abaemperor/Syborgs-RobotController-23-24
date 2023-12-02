15f035d031d513bff4b576e43a0db080f49df4fc
HackerGuy1000 <107278740+HackerGuy1000@users.noreply.github.com>
2023-12-01 22:56:27 -0500

    Created a class for the Intake with methods to open close and spin

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/intake/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/intake/Intake.java
@@ -1,7 +1,31 @@
 package org.firstinspires.ftc.teamcode.subsystem.intake;
 
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Intake {
-    public DcMotor intakeMotor;
+    public DcMotor intakeLift;
+    public CRServo intakeSpin;
+
+    public final int INTAKE_CLOSE = 0;
+    public final int INTAKE_OPEN = 0;
+
+    public Intake(HardwareMap hardwareMap){
+        intakeLift = hardwareMap.get(DcMotor.class,"IL");
+        intakeSpin = hardwareMap.get(CRServo.class, "IS");
+
+        intakeLift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+    public void close(){
+        intakeLift.setTargetPosition(INTAKE_CLOSE);
+        intakeLift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    }
+    public void open(){
+        intakeLift.setTargetPosition(INTAKE_OPEN);
+        intakeLift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    }
+    public void spin(double power){
+        intakeSpin.setPower(power);
+    }
 }
